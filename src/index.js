0017961db38810d4cf888c1d0467466a31f5a795
KSGitUser <rusakk@mail.ru>
2021-03-15 22:12:52 +0300

    feat: check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let result = [];
+    const equalBrackets = [];
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+    str.split('').map(char => {
+        bracketsConfig.map(brArrow => {
+            if (brArrow[0] === char) {
+                if (brArrow[0] === brArrow[1]) {
+                    if (!equalBrackets.includes(brArrow[0])) {
+                        equalBrackets.push(brArrow[0]);
+                    }
+                    if (result.length && result[result.length - 1] === brArrow[0]) {
+                        result.pop();
+                        return;
+                    }
+                    result.push(char);
+                    return;
+                }
+                result.push(char);
+                return;
+            } else {
+                if (brArrow[1] === char) {
+                    try {
+                        if (brArrow[0] === result[result.length - 1]) {
+                            result.pop();
+                        }
+                    } catch (e) {
+                        return false;
+                    }
+                }
+            }
+        })
+    });
+
+    let finallResult = [];
+
+    if (result.length && equalBrackets.length) {
+        for (let index = 0; index < result.length; index++) {
+            let item = result[index];
+            if ([...result.slice(index + 1)].includes(item)) {
+                return false;
+            }
+            if (!equalBrackets.includes(item)) {
+                finallResult.push(item);
+            }
+        }
+        result = [];
+    }
+
+    return result.length === 0 && finallResult.length === 0 ? true : false;
 }
